744e84707673be0467d9cd2c7f339ee2f8482757
Thien Ly <thien.ly@groovetechnology.com>
2024-06-26 12:49:58 +0700

    switch to esmodule

--- src/message-listener.ts
+++ src/message-listener.ts
@@ -1,5 +1,5 @@
 import { APIV1, Message } from "./api-v1";
-import { Observable } from "./lib/observable";
+import { Observable, Observer } from "./lib/observable";
 import WebSocket from "ws";
 
 export class MessageListener {
@@ -9,12 +9,14 @@ export class MessageListener {
     this.observable = new Observable<Message>();
     this.ws = new WebSocket(`ws://${api.baseUrl}/monitor/messages`);
     this.ws.addEventListener("message", ({ data }) => {
-      (data as unknown as Message[]).forEach((msg: Message) => {
-        this.observable.notify(`message:${msg.id}`, msg);
+      this.observable.notify("message", data as unknown as Message);
+
+      this.ws.once("close", () => {
+        this.observable.unsubscribe();
       });
-      this.ws.once('close',()=>{
-        this.observable.unsubscribe()
-      })
     });
+  }
+  public listen(listener: Observer<Message>) {
+    return this.observable.subscribe("message", listener);
   }
 }
